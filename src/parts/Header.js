5f8db1803c612e39b068ef63934c6f72db90eda8
Dion Gultom <68895186+diongultom@users.noreply.github.com>
2021-04-21 23:58:13 +0700

    header

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -7,30 +7,31 @@ export default function Header(props) {
   const getNavLinkClass = (path) => {
     return props.location.pathname === path ? " active" : "";
   };
+
   return (
-    <header className="Spacing-sm">
+    <header className="spacing-sm">
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light">
           <BrandIcon />
 
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav ml-auto">
-              <li className={'nav-item$get{getNavLinkClass("/")'}>
-                <Button className="nav-link" type="link" href="">
+              <li className={`nav-item${getNavLinkClass("/")}`}>
+                <Button className="nav-link" type="link" href="/">
                   Home
                 </Button>
               </li>
-              <li className={'nav-item$get{getNavLinkClass("/browse-by")'}>
+              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
                 <Button className="nav-link" type="link" href="/browse-by">
                   Browse By
                 </Button>
               </li>
-              <li className={'nav-item$get{getNavLinkClass("/stories")'}>
+              <li className={`nav-item${getNavLinkClass("/stories")}`}>
                 <Button className="nav-link" type="link" href="/stories">
                   Stories
                 </Button>
               </li>
-              <li className={'nav-item$get{getNavLinkClass("/agents")'}>
+              <li className={`nav-item${getNavLinkClass("/agents")}`}>
                 <Button className="nav-link" type="link" href="/agents">
                   Agents
                 </Button>
