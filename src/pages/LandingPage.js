ecdfbd5342e83d915b4f0ceb315315ed10823ab8
Dion Gultom <68895186+diongultom@users.noreply.github.com>
2021-06-26 10:15:27 +0700

    footer and fade

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
@@ -12,7 +14,6 @@ export default class LandingPage extends Component {
     this.refMostPicked = React.createRef();
   }
   render() {
-    console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>
@@ -22,6 +23,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
       </>
     );
   }
